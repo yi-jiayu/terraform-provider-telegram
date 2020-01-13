48b03e0d8ce091f357621f6ab65b5d6fbb1c6afa
Jiayu Yi <yijiayu@gmail.com>
2020-01-13 17:55:35 +0800

    Add certificate argument to telegram_bot_webhook resource (#3)
    * Add certificate argument to telegram_bot_webhook resource
    
    * Remove certificate argument from main.tf
    
    Closes #1

--- telegram/resource_telegram_bot_webhook.go
+++ telegram/resource_telegram_bot_webhook.go
@@ -27,6 +27,10 @@ func resourceTelegramBotWebhook() *schema.Resource {
 				Type:     schema.TypeInt,
 				Optional: true,
 			},
+			"has_custom_certificate": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -34,7 +38,11 @@ func resourceTelegramBotWebhook() *schema.Resource {
 func resourceTelegramBotWebhookCreate(d *schema.ResourceData, m interface{}) error {
 	botAPI := m.(*tgbotapi.BotAPI)
 	url := d.Get("url").(string)
-	result, err := botAPI.SetWebhook(tgbotapi.NewWebhook(url))
+	config := tgbotapi.NewWebhook(url)
+	if cert, ok := d.Get("certificate").(string); ok && cert != "" {
+		config.Certificate = tgbotapi.FileBytes{Bytes: []byte(cert)}
+	}
+	result, err := botAPI.SetWebhook(config)
 	if err != nil {
 		return err
 	}
@@ -42,7 +50,7 @@ func resourceTelegramBotWebhookCreate(d *schema.ResourceData, m interface{}) err
 		return errors.New(result.Description)
 	}
 	d.SetId(strconv.Itoa(botAPI.Self.ID))
-	return nil
+	return resourceTelegramBotWebhookRead(d, m)
 }
 
 func resourceTelegramBotWebhookRead(d *schema.ResourceData, m interface{}) error {
@@ -56,7 +64,13 @@ func resourceTelegramBotWebhookRead(d *schema.ResourceData, m interface{}) error
 		d.SetId("")
 		return nil
 	}
-	return d.Set("url", url)
+	if err := d.Set("url", url); err != nil {
+		return err
+	}
+	if err := d.Set("has_custom_certificate", info.HasCustomCertificate); err != nil {
+		return err
+	}
+	return nil
 }
 
 func resourceTelegramBotWebhookUpdate(d *schema.ResourceData, m interface{}) error {
